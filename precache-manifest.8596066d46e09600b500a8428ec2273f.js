8b1c32d2f37815673a57632c27896b19193762b6
Rodolfo Vieira <rodolfovieira95@outlook.com>
2020-07-02 00:19:39 -0300

    Updates

---
+++ precache-manifest.8596066d46e09600b500a8428ec2273f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd3580e3edd54a32e3122cc90ef38819",
+    "url": "/learning-hooks/index.html"
+  },
+  {
+    "revision": "d1987cecf32fb12f1242",
+    "url": "/learning-hooks/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "48d24fc9555c84fe7bc4",
+    "url": "/learning-hooks/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d1987cecf32fb12f1242",
+    "url": "/learning-hooks/static/js/2.c39826a6.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/learning-hooks/static/js/2.c39826a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48d24fc9555c84fe7bc4",
+    "url": "/learning-hooks/static/js/main.8ee067a1.chunk.js"
+  },
+  {
+    "revision": "618f8f9b15b0be80a5e5",
+    "url": "/learning-hooks/static/js/runtime-main.6594113e.js"
+  }
+]);
